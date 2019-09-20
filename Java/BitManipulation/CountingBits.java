2ac2244b6ef1ae81981777e5de736523c0879db5
fords <zxw6805@rit.edu>
2019-09-20 14:00:00 +0100

    Add topics by study time

--- Java/BitManipulation/CountingBits.java
+++ Java/BitManipulation/CountingBits.java
@@ -15,3 +15,23 @@ Follow up:
 It is very easy to come up with a solution with run time O(n*sizeof(integer)).
 But can you do it in linear time O(n) /possibly in a single pass?
 */
+class CountingBits {
+    public int bruteforce(int[] nums){
+
+    }
+
+    public int missingNumber(int[] nums) {
+      int i = 0;
+      int b = 1;
+      int[] dp = new int[num+1];
+      while(b <= num){
+        while (i < b && i + B <= num){
+          dp[i+b] = dp[i] + 1;
+          i ++;
+        }
+        i = 0;
+        b = b << 1;
+      }
+    }
+
+}
